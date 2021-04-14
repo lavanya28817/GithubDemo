5b76a47f16f94b6be5991057f100443480c30148
HOME <HOME@HOME-PC>
2021-04-14 13:23:45 +0530

    First commit

---
+++ src/GithubDemo.java
@@ -0,0 +1,14 @@
+
+public class GithubDemo {
+	public static void main(String[] args) {
+		String hungry="yes";
+		if(hungry=="yes")
+		{
+			System.out.println("Eat something....");
+		}else {
+				System.out.println("Do Work....");
+			}
+		}
+	}
+
+
